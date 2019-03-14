c753701096221e8c1935af8456f6f7733497b119
houyanjing <houyanjing4@sina.com>
2019-03-14 17:56:11 +0800

    vff

---
+++ SZBleSDK.framework/Headers/SZBluetoothManager.h
@@ -0,0 +1,48 @@
+//
+//  SZBluetoothManager.h
+//  SZBleSDK
+//
+//  Created by 侯艳静 on 2017/12/28.
+//  Copyright © 2017年 houyanjing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SZBleDelegate<NSObject>
+
+-(void)openSuccecc:(NSDictionary *)dic;
+
+-(void)GPRSBackgroundOpenDoor:(NSDictionary *)dic;
+
+-(void)BLEListOpenDoor:(NSDictionary *)dic;
+
+
+@end
+
+@interface SZBluetoothManager : NSObject
+/*
+ *当前开门返回的数据信息
+ */
+@property (nonatomic, strong) NSString * message;
+/*
+ *开门数据回调
+ */
+@property (nonatomic, weak) id<SZBleDelegate>delegate;
+/*
+ *蓝牙开锁SDK管理
+ */
++(SZBluetoothManager *)shareManager;
+
+- (void)aboutKeyList:(NSArray *)arr;
+
+-(void)opendoor:(NSDictionary *)dic;
+//蓝牙列表开门 所搜索到的蓝牙设备
+-(void)aboutBLEListOpenDoor:(NSDictionary *)dic;
+//通过选中列表值开门
+-(void)aboutBLEListPer:(NSString *)str;
+//自动开锁开关
+-(void)lockSwitch:(BOOL)isTrue;
+
+-(BOOL)szsaveUserPlot_id:(NSString *)plot_id;
+
+@end
